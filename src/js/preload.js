29f543ca2fb23c930f3f7b06d054348127f3aa6d
adrienb39 <adrienb39@yahoo.com>
2025-04-02 09:31:16 +0200

    ajout de inscription dans le preload

--- src/js/preload.js
+++ src/js/preload.js
@@ -4,3 +4,7 @@ contextBridge.exposeInMainWorld('versions', {
     // Fonction qui récupère les versions via IPC
     getVersions: () => ipcRenderer.invoke('get-versions')
 })
+
+contextBridge.exposeInMainWorld('inscription', {
+    add: (nom, prenom, email, password) => ipcRenderer.invoke('inscription:add', {nom, prenom, email, password})
+})
