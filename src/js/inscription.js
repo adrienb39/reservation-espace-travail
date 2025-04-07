197def069c702adf530a7cf784770d1f94dc149c
adrienb39 <adrienb39@yahoo.com>
2025-04-07 10:45:52 +0200

    ajout de la validation de l'email

--- src/js/inscription.js
+++ src/js/inscription.js
@@ -7,7 +7,18 @@ formSignup.addEventListener("submit", async (e) => {
     let nom = document.querySelector("#nom").value
     let prenom = document.querySelector("#prenom").value
     let email = document.querySelector("#email").value
+    const erreurMessageEmail = document.querySelector("#erreur-message-email");
     try {
+        const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (!pattern.test(email)) {
+            erreurMessageEmail.classList.add('d-block', 'text-danger')
+            erreurMessageEmail.textContent = "Email invalide"
+            return
+        } else {
+            erreurMessageEmail.classList.remove('d-block')
+            erreurMessageEmail.classList.add('d-none')
+        }
+
         await user.addUser(nom, prenom, email)
     } catch (error) {
         console.error(error)
