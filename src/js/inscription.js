4fda5c2a650762e700884808169af505a53c1ac2
adrienb39 <adrienb39@yahoo.com>
2025-05-13 12:09:14 +0200

    ajout du hashage du mot de passe

--- src/js/inscription.js
+++ src/js/inscription.js
@@ -1,5 +1,7 @@
 // Processus de rendu
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 let formSignup = document.querySelector('#form-signup')
 
 formSignup.addEventListener("submit", async (e) => {
@@ -10,9 +12,20 @@ formSignup.addEventListener("submit", async (e) => {
     let password = document.querySelector("#password").value
     let confirmPassword = document.querySelector("#confirmPassword").value
     try {
+        const hashPassword = async (plainPassword) => {
+            try {
+                const salt = await bcrypt.genSalt(saltRounds);
+                const hashedPassword = await bcrypt.hash(plainPassword, salt);
+                console.log('Mot de passe haché :', hashedPassword);
+                return hashedPassword;
+            } catch (error) {
+                console.error('Erreur lors du hachage du mot de passe :', error);
+                throw error;
+            }
+        };
         formSignup.classList.add('was-validated')
         if (formSignup.checkValidity()) {
-            await user.addUser(nom, prenom, email)
+            await user.addUser(nom, prenom, email, hashPassword(password))
         }
     } catch (error) {
         console.error(error)
