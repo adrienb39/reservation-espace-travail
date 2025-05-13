2095a58f5cad76766209c8d50d3bcfcd491f9b99
adrienb39 <adrienb39@yahoo.com>
2025-05-13 16:41:52 +0200

    ajout de la vérification entre le mot de passe et la confirmation du mot de passe

--- src/js/inscription.js
+++ src/js/inscription.js
@@ -9,10 +9,17 @@ formSignup.addEventListener("submit", async (e) => {
     let email = document.querySelector("#email").value
     let password = document.querySelector("#password").value
     let confirmPassword = document.querySelector("#confirmPassword").value
+    let error = document.querySelector('#error')
     try {
-        formSignup.classList.add('was-validated')
-        if (formSignup.checkValidity()) {
-            await user.addUser(nom, prenom, email, password)
+        if (password != confirmPassword) {
+            error.textContent = 'Les mots de passe ne correspondent pas'
+            error.classList.add('text-danger')
+        } else {
+            formSignup.classList.add('was-validated')
+            error.textContent = ''
+            if (formSignup.checkValidity()) {
+                await user.addUser(nom, prenom, email, password)
+            }
         }
     } catch (error) {
         console.error(error)
